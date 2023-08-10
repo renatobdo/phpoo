d6243ca8e5d3dd0238ed10322d76a4256e9f049b
Renato Bueno <renatobdo@gmail.com>
2023-08-10 16:25:59 -0300

    Classe Usuario cafebistro
    Classe Usuario cafebistro

---
+++ phpoo_turmaA/usuario.sql
@@ -0,0 +1,9 @@
+create database cafebistro;
+use cafebistro;
+create table usuario (id int primary key,
+nome varchar(40),
+email varchar(255),
+senha varchar(255));
+insert into usuario values (1, 'admin', 
+'adminturmaa@ifsp.edu.br', 'turmaa');
+select * from usuario;
