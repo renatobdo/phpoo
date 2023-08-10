3d79c4e1c6ca5cd685ec9dae996dcddaaab4a786
Renato Bueno <renatobdo@gmail.com>
2023-08-10 15:03:35 -0300

    Atualização de conteúdo
    Atualização de conteúdo

---
+++ phpoo_turmaA/formulario_carro.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo de Formulário do carro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+<body>
+    <div class="container">
+        <h3>Preencha os Dados</h3>
+        <form method="post">
+            <div class="form-group">
+                <label for="marca">marca:</label>
+                <input type="text" class="form-control" id="marca" name="marca" required>
+            </div>
+            <div class="form-group">
+                <label for="modelo">Modelo:</label>
+                <input type="text" class="form-control" id="modelo" name="modelo" required>
+            </div>
+            <div class="form-group">
+                <label for="ano">Ano:</label>
+                <input type="date" class="form-control" id="ano" name="ano" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        
+        <?php
+        include 'Carro.php';
+        
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $marca = $_POST["marca"];
+            $modelo = $_POST["modelo"];
+            $ano = $_POST["ano"];
+            
+            
+            $carro = new Carro($marca, $modelo, $ano);
+            
+            echo "<h3>Dados Enviados</h3>";
+            echo "<p><strong>Marca:</strong> " . $carro->get_marca() . "</p>";
+            echo "<p><strong>Modelo:</strong> " . $carro->get_modelo() . "</p>";
+            echo "<p><strong>Ano:</strong> " . $carro->get_ano() . "</p>";
+        }
+        ?>
+    </div>
+</body>
+</html>
