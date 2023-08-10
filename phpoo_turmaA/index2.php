cd11a35ab97b1afc1da20e02cf3da7887f6ac25a
Renato Bueno <renatobdo@gmail.com>
2023-08-10 14:32:19 -0300

    Atualização de conteúdo
    Atualização de conteúdo

---
+++ phpoo_turmaA/index2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo de Formulário e Classe Pessoa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+<body>
+    <div class="container">
+        <h3>Preencha os Dados</h3>
+        <form method="post">
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input type="text" class="form-control" id="nome" name="nome" required>
+            </div>
+            <div class="form-group">
+                <label for="idade">Idade:</label>
+                <input type="number" class="form-control" id="idade" name="idade" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+</div>
+</body>
+</html>
