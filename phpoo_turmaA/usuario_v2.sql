1c97b98133e92599ae4517b9745b122fb2220191
Renato Bueno <renatobdo@gmail.com>
2023-08-17 16:39:04 -0300

    Atualização de conteúdo
    Atualização de conteúdo

---
+++ phpoo_turmaA/usuario_v2.sql
@@ -0,0 +1,7 @@
+create database cafebistro_phpoo_turmaA;
+use cafebistro_phpoo_turmaA;
+create table usuario (id int 
+auto_increment primary key,
+nome varchar(255),
+email varchar(255),
+senha varchar(255));
