308dd4b2a3dbaf8b17e1ea7cbae73a2544e50840
Renato Bueno <renatobdo@gmail.com>
2023-08-18 06:24:28 -0300

    Tela de cadastro
    Tela de cadastro

--- phpoo_turmaA/usuario_v2.sql
+++ phpoo_turmaA/usuario_v2.sql
@@ -1,7 +1,8 @@
-create database cafebistro_phpoo_turmaA;
-use cafebistro_phpoo_turmaA;
+create database cafebistro_phpoo_turmaB;
+use cafebistro_phpoo_turmaB;
 create table usuario (id int 
 auto_increment primary key,
 nome varchar(255),
 email varchar(255),
 senha varchar(255));
+select * from usuario;
