0d164412f468bdc0ca0b27b0f5bdd42ae73b6db5
Renato Bueno <renatobdo@gmail.com>
2023-08-24 07:11:49 -0300

    Atualização de conteúdo
    Atualização de conteúdo

--- phpoo_turmaA/Semana25/CafeBistro/usuario_v2.sql
+++
@@ -1,8 +0,0 @@
-create database cafebistro_phpoo_turmaa;
-use cafebistro_phpoo_turmaa;
-create table usuario (
-nome varchar(255),
-email varchar(255) primary key,
-senha varchar(255));
-select * from usuario;
-
